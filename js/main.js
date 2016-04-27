66dc400f9a4fb44435944f2ab168c6fc2911e2bc
swalljasper <swalljasper@wisc.edu>
2016-04-27 16:51:17 -0500

    add polyline in leaflet

--- js/main.js
+++ js/main.js
@@ -22,169 +22,197 @@ function createMap(){
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
         subdomains: 'abcd'
     }).addTo(map);
+
+    getData(map);
+
+
 };
+
+
 
 //Import GeoJSON data
 function getData(map){
     //load the data
-    $.ajax("data/topojsons/BlueLine.topojson", {
-        dataType: "json"
+   L.TopoJSON = L.GeoJSON.extend({  
+      addData: function(jsonData) {    
+        if (jsonData.type === "Topology") {
+          for (key in jsonData.objects) {
+            geojson = topojson.feature(jsonData, jsonData.objects[key]);
+            L.GeoJSON.prototype.addData.call(this, geojson);
+          }
+        }    
+        else {
+          L.GeoJSON.prototype.addData.call(this, jsonData);
+        }
+      }  
     });
+
+   var topoLayer = new L.TopoJSON();
+
+$.getJSON('data/topojsons/BlueLine.topojson')
+  .done(addTopoData);
+
+function addTopoData(topoData){  
+  topoLayer.addData(topoData);
+  topoLayer.addTo(map);
+}
 };
+
+
     // use queue.js to parallelize asynchronous data loading
-    d3_queue.queue()
-        .defer(d3.csv, "data/TransitData.csv") //load attributes from csv
-        .defer(d3.json, "data/topojsons/MetroCensusTracts.topojson")
-        .defer(d3.json, "data/topojsons/LakesAndRivers.topojson")
-        .defer(d3.json, "data/topojsons/BlueLine.topojson")
-        .defer(d3.json, "data/topojsons/BlueLineExt.topojson")
-        .defer(d3.json, "data/topojsons/GoldLine.topojson")
-        .defer(d3.json, "data/topojsons/GreenLine.topojson")
-        .defer(d3.json, "data/topojsons/GreenLineExt.topojson")
-        .defer(d3.json, "data/topojsons/NorthStarLine.topojson")
-        .defer(d3.json, "data/topojsons/OrangeLine.topojson")
-        .defer(d3.json, "data/topojsons/RedLine.topojson")
-        .defer(d3.json, "data/topojsons/RedLineExt.topojson")
-        .defer(d3.json, "data/topojsons/BlueStations.topojson")
-        .defer(d3.json, "data/topojsons/GreenStations.topojson")
-        .defer(d3.json, "data/topojsons/RedStations.topojson")
-        .defer(d3.json, "data/topojsons/NorthStarStations.topojson")
-        .defer(d3.json, "data/topojsons/SharedStations.topojson")
-        .await(callback);
+//     d3_queue.queue()
+//         .defer(d3.csv, "data/TransitData.csv") //load attributes from csv
+//         .defer(d3.json, "data/topojsons/MetroCensusTracts.topojson")
+//         .defer(d3.json, "data/topojsons/LakesAndRivers.topojson")
+//         .defer(d3.json, "data/topojsons/BlueLine.topojson")
+//         .defer(d3.json, "data/topojsons/BlueLineExt.topojson")
+//         .defer(d3.json, "data/topojsons/GoldLine.topojson")
+//         .defer(d3.json, "data/topojsons/GreenLine.topojson")
+//         .defer(d3.json, "data/topojsons/GreenLineExt.topojson")
+//         .defer(d3.json, "data/topojsons/NorthStarLine.topojson")
+//         .defer(d3.json, "data/topojsons/OrangeLine.topojson")
+//         .defer(d3.json, "data/topojsons/RedLine.topojson")
+//         .defer(d3.json, "data/topojsons/RedLineExt.topojson")
+//         .defer(d3.json, "data/topojsons/BlueStations.topojson")
+//         .defer(d3.json, "data/topojsons/GreenStations.topojson")
+//         .defer(d3.json, "data/topojsons/RedStations.topojson")
+//         .defer(d3.json, "data/topojsons/NorthStarStations.topojson")
+//         .defer(d3.json, "data/topojsons/SharedStations.topojson")
+//         .await(callback);
 
-        function callback(error, transitData, censustracts, lakes, blueline, bluelineext, goldline, greenline, greenlineext, northstarline, orangeline, redline, redlineext, bluestations, greenstations, redstations, northstarstations, sharedstations){
-        //translate europe TopoJSON
-        var censusTracts = topojson.feature(censustracts, censustracts.objects.MetroCensusTracts),
-            lakesAndRivers = topojson.feature(lakes, lakes.objects.LakesAndRivers),
-            blueLine = topojson.feature(blueline, blueline.objects.BlueLine),
-        	blueLineExt = topojson.feature(bluelineext, bluelineext.objects.BlueLineExt),
-        	goldLine = topojson.feature(goldline, goldline.objects.GoldLine),
-        	greenLine = topojson.feature(greenline, greenline.objects.GreenLine),
-        	greenLineExt = topojson.feature(greenlineext, greenlineext.objects.GreenLineExt),
-        	northStarLine = topojson.feature(northstarline, northstarline.objects.NorthStarLine),
-        	orangeLine = topojson.feature(orangeline, orangeline.objects.OrangeLine)
-        	redLine = topojson.feature(redline, redline.objects.RedLine),
-        	redLineExt = topojson.feature(redlineext, redlineext.objects.RedLineExt),
-            blueStations = topojson.feature(bluestations, bluestations.objects.BlueStations),
-            greenStations = topojson.feature(greenstations, greenstations.objects.GreenStations),
-            redStations = topojson.feature(redstations, redstations.objects.RedStations),
-            northStarStations = topojson.feature(northstarstations, northstarstations.objects.NorthStarStations),
-            sharedStations = topojson.feature(sharedstations, sharedstations.objects.SharedStations);
-
-
-        //add geojsons to map
-        var tracts = map.append("path")
-            .datum(censusTracts)
-            .attr("class", "tracts")
-            .attr("d", path);
-
-        var hydro = map.append("path")
-            .datum(lakesAndRivers)
-            .attr("class", "hydro")
-            .attr("d", path);
-
-        var blue = map.append("path")
-            .datum(blueLine)
-            .attr("class", "blue")
-            .attr("d", path);
-
-        var blueext = map.append("path")
-            .datum(blueLineExt)
-            .attr("class", "blueext")
-            .attr("d", path);
-
-        var gold = map.append("path")
-            .datum(goldLine)
-            .attr("class", "gold")
-            .attr("d", path);
-
-        var green = map.append("path")
-            .datum(greenLine)
-            .attr("class", "green")
-            .attr("d", path);
-
-        var greenext = map.append("path")
-            .datum(greenLineExt)
-            .attr("class", "greenext")
-            .attr("d", path);
-
-        var northstar3 = map.append("path")
-            .datum(northStarLine)
-            .attr("class", "northstar3")
-            .attr("d", path);
-
-        var northstar2 = map.append("path")
-            .datum(northStarLine)
-            .attr("class", "northstar2")
-            .attr("d", path);
-
-        var northstar = map.append("path")
-            .datum(northStarLine)
-            .attr("class", "northstar")
-            .attr("d", path);
-
-        var orange = map.append("path")
-            .datum(orangeLine)
-            .attr("class", "orange")
-            .attr("d", path);
-
-        var red = map.append("path")
-            .datum(redLine)
-            .attr("class", "red")
-            .attr("d", path);
-
-        var redext = map.append("path")
-            .datum(redLineExt)
-            .attr("class", "redext")
-            .attr("d", path);
+//         function callback(error, transitData, censustracts, lakes, blueline, bluelineext, goldline, greenline, greenlineext, northstarline, orangeline, redline, redlineext, bluestations, greenstations, redstations, northstarstations, sharedstations){
+//         //translate europe TopoJSON
+//         var censusTracts = topojson.feature(censustracts, censustracts.objects.MetroCensusTracts),
+//             lakesAndRivers = topojson.feature(lakes, lakes.objects.LakesAndRivers),
+//             blueLine = topojson.feature(blueline, blueline.objects.BlueLine),
+//         	blueLineExt = topojson.feature(bluelineext, bluelineext.objects.BlueLineExt),
+//         	goldLine = topojson.feature(goldline, goldline.objects.GoldLine),
+//         	greenLine = topojson.feature(greenline, greenline.objects.GreenLine),
+//         	greenLineExt = topojson.feature(greenlineext, greenlineext.objects.GreenLineExt),
+//         	northStarLine = topojson.feature(northstarline, northstarline.objects.NorthStarLine),
+//         	orangeLine = topojson.feature(orangeline, orangeline.objects.OrangeLine)
+//         	redLine = topojson.feature(redline, redline.objects.RedLine),
+//         	redLineExt = topojson.feature(redlineext, redlineext.objects.RedLineExt),
+//             blueStations = topojson.feature(bluestations, bluestations.objects.BlueStations),
+//             greenStations = topojson.feature(greenstations, greenstations.objects.GreenStations),
+//             redStations = topojson.feature(redstations, redstations.objects.RedStations),
+//             northStarStations = topojson.feature(northstarstations, northstarstations.objects.NorthStarStations),
+//             sharedStations = topojson.feature(sharedstations, sharedstations.objects.SharedStations);
 
 
+//         //add geojsons to map
+//         var tracts = map.append("path")
+//             .datum(censusTracts)
+//             .attr("class", "tracts")
+//             .attr("d", path);
 
-        var bluestat = map.selectAll(".bluestat")
-            .data(blueStations.features)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "bluestat " + d.StationID;
-            })
-            .attr("d", path);
+//         var hydro = map.append("path")
+//             .datum(lakesAndRivers)
+//             .attr("class", "hydro")
+//             .attr("d", path);
+
+//         var blue = map.append("path")
+//             .datum(blueLine)
+//             .attr("class", "blue")
+//             .attr("d", path);
+
+//         var blueext = map.append("path")
+//             .datum(blueLineExt)
+//             .attr("class", "blueext")
+//             .attr("d", path);
+
+//         var gold = map.append("path")
+//             .datum(goldLine)
+//             .attr("class", "gold")
+//             .attr("d", path);
+
+//         var green = map.append("path")
+//             .datum(greenLine)
+//             .attr("class", "green")
+//             .attr("d", path);
+
+//         var greenext = map.append("path")
+//             .datum(greenLineExt)
+//             .attr("class", "greenext")
+//             .attr("d", path);
+
+//         var northstar3 = map.append("path")
+//             .datum(northStarLine)
+//             .attr("class", "northstar3")
+//             .attr("d", path);
+
+//         var northstar2 = map.append("path")
+//             .datum(northStarLine)
+//             .attr("class", "northstar2")
+//             .attr("d", path);
+
+//         var northstar = map.append("path")
+//             .datum(northStarLine)
+//             .attr("class", "northstar")
+//             .attr("d", path);
+
+//         var orange = map.append("path")
+//             .datum(orangeLine)
+//             .attr("class", "orange")
+//             .attr("d", path);
+
+//         var red = map.append("path")
+//             .datum(redLine)
+//             .attr("class", "red")
+//             .attr("d", path);
+
+//         var redext = map.append("path")
+//             .datum(redLineExt)
+//             .attr("class", "redext")
+//             .attr("d", path);
+
+
+
+//         var bluestat = map.selectAll(".bluestat")
+//             .data(blueStations.features)
+//             .enter()
+//             .append("path")
+//             .attr("class", function(d){
+//                 return "bluestat " + d.StationID;
+//             })
+//             .attr("d", path);
             
-        var greenstat = map.selectAll(".greenstat")
-            .data(greenStations.features)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "greenstat " + d.StationID;
-            })
-            .attr("d", path);  
+//         var greenstat = map.selectAll(".greenstat")
+//             .data(greenStations.features)
+//             .enter()
+//             .append("path")
+//             .attr("class", function(d){
+//                 return "greenstat " + d.StationID;
+//             })
+//             .attr("d", path);  
 
-        var redstat = map.selectAll(".redstat")
-            .data(redStations.features)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "redstat " + d.StationID;
-            })
-            .attr("d", path);  
+//         var redstat = map.selectAll(".redstat")
+//             .data(redStations.features)
+//             .enter()
+//             .append("path")
+//             .attr("class", function(d){
+//                 return "redstat " + d.StationID;
+//             })
+//             .attr("d", path);  
 
-        var northstarstat = map.selectAll(".northstarstat")
-            .data(northStarStations.features)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "northstarstat " + d.StationID;
-            })
-            .attr("d", path);  
+//         var northstarstat = map.selectAll(".northstarstat")
+//             .data(northStarStations.features)
+//             .enter()
+//             .append("path")
+//             .attr("class", function(d){
+//                 return "northstarstat " + d.StationID;
+//             })
+//             .attr("d", path);  
 
-        var sharedstat = map.selectAll(".sharedstat")
-            .data(sharedStations.features)
-            .enter()
-            .append("path")
-            .attr("class", function(d){
-                return "sharedstat " + d.StationID;
-            })
-            .attr("d", path);     
-};
+//         var sharedstat = map.selectAll(".sharedstat")
+//             .data(sharedStations.features)
+//             .enter()
+//             .append("path")
+//             .attr("class", function(d){
+//                 return "sharedstat " + d.StationID;
+//             })
+//             .attr("d", path);     
+// };
 
     // var attrArray = ["Median age (years)", "Households", "Kids Under 18", "Car, truck, or van - Mean travel time to work (minutes)", "Public transportation - Mean travel time to work (minutes)", "Drove Alone", "Carpooled", "Public transportation (excluding taxicab)", "Bicycle", "Walked", "Other means", "Worked at home", "White", "Black or African American", "American Indian and Alaska Native", "Asian", "Native Hawaiian and Other Pacific Islander", "Some other race alone", "Two or more races", "Median household income"]; 
 
