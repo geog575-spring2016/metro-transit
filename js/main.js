4f38d319d0e077a4bc39f37996bed5b8ba007744
cjmorgan3 <cjmorgan3@wisc.edu>
2016-05-03 12:21:57 -0500

    Black & White

--- js/main.js
+++ js/main.js
@@ -56,13 +56,13 @@ function getData(map){
   L.geoJson( censusTracts ).addTo(map);
 
   function getColor(d) {
-    return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
+    return d > 10000 ? '#000000' :
+           d > 8000  ? '#2B2B2B' :
+           d > 6000  ? '#555555' :
+           d > 4000  ? '#808080' :
+           d > 2000   ? '#AAAAAA' :
+           d > 1000   ? '#D5D5D5' :
+           d > 500   ? '#FFFFFF' :
                       '#FFEDA0';
   }
   function style(feature) {
@@ -72,7 +72,7 @@ function getData(map){
         opacity: 1,
         color: 'white',
         dashArray: '3',
-        fillOpacity: 0.7
+        fillOpacity: 0.6
     };
   }
 
